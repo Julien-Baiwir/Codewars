dce6f00c643aacdec56ce71f444aeb7ffced74c6
Julien-Baiwir <julien.freddy.baiwir@gmail.com>
2024-01-28 19:55:40 +0100

    py

--- script.py
+++ script.py
@@ -1,11 +1,13 @@
-def remove_char(s):
-  return s[1:-1]
+def positive_sum(arr):
+    new_array = list(filter(lambda x: x >= 0, arr))
+    if sum(new_array) > 0:
+        return sum(new_array)
+    else:
+        return 0
 
+arr = [1, -2, 3, 4, 5]
+print(positive_sum(arr))
 
-# def remove_char(s):
-#     s = list(s)
-#     s.pop()
-#     s.pop(0)
-#     return ''.join(s)
-
-
+# best practice
+# def positive_sum(arr):
+#     return sum(x for x in arr if x > 0)
