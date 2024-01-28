89e2ba5436d6513cab63c8f9bf2f318dba618182
Julien-Baiwir <julien.freddy.baiwir@gmail.com>
2024-01-28 18:35:02 +0100

    r

--- script.py
+++ script.py
@@ -1,22 +1,11 @@
-def better_than_average(class_points, your_points):
-
- average__class__points = sum(class_points) / len(class_points) 
-
- if average__class__points> your_points:
-  return False
-
- else: 
-  return True
-
-class_points= [10, 14, 18]
-your_points= 15
-print(better_than_average(class_points, your_points))
-
-# best practice
-# def better_than_average(class_points, your_points):
-#     return your_points > sum(class_points) / len(class_points)
+def remove_char(s):
+  return s[1:-1]
 
 
-# import statistics
-# def better_than_average(class_points, your_points):
-#     return your_points > statistics.mean(class_points)
+# def remove_char(s):
+#     s = list(s)
+#     s.pop()
+#     s.pop(0)
+#     return ''.join(s)
+
+
