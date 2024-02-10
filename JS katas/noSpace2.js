bf158261959d88a6b80190cf822d03a781ee58e2
Julien-Baiwir <julien.freddy.baiwir@gmail.com>
2024-02-10 13:44:26 +0100

    ok

--- JS katas/noSpace2.js
+++ JS katas/noSpace2.js
@@ -15,3 +15,7 @@ console.log(noSpace(x));
 
 // Dans le premier cas (x.split('').join('')), chaque caractère est considéré comme un élément distinct, donc aucun caractère n'est supprimé, y compris les espaces.
 // Dans le deuxième cas (x.split(' ').join('')), la chaîne est divisée en sous-chaînes à chaque espace, et ensuite ces sous-chaînes sont réassemblées sans espace, ce qui supprime tous les espaces de la chaîne d'origine.
+
+function noSpace(x) {
+  return x.replaceAll(' ', '');
+}
